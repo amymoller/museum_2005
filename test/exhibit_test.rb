0e259a632889a6e9eeece5b1088314dd353e8419
Amy Moller <amy.moller@yahoo.com>
2020-05-27 09:28:46 -0600

    Add last exhibit test

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -5,9 +5,24 @@ require './lib/exhibit'
 class ExhibitTest < Minitest::Test
 
   def setup
-    exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
+
+    @exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
   end
 
+  def test_it_exists
+
+    assert_instance_of Exhibit, @exhibit
+  end
+
+  def test_it_has_a_name
+    skip
+    assert_equal "Gems and Minerals", @exhibit.name
+  end
+
+  def test_it_has_no_cost
+
+    assert_equal 0, @exhibit.cost
+  end
 
 
 
