e782f9ea953939724c1a4b5c35c2c6cc939728f9
Amy Moller <amy.moller@yahoo.com>
2020-05-27 10:25:38 -0600

    Add more tests

--- test/museum_test.rb
+++ test/museum_test.rb
@@ -9,6 +9,9 @@ class MuseumTest < Minitest::Test
   def setup
 
     @dmns = Museum.new({name: "Denver Museum of Nature and Science"})
+    @gems_and_minerals = Exhibit.new({name: "Gems and Minerals", cost: 0})
+    @dead_sea_scrolls = Exhibit.new({name: "Dead Sea Scrolls", cost: 10})
+    @imax = Exhibit.new({name: "IMAX",cost: 15})
   end
 
   def test_it_exists
@@ -21,9 +24,25 @@ class MuseumTest < Minitest::Test
     assert_equal "Denver Museum of Nature and Science", @dmns.name
   end
 
+  def test_it_has_exhibits
+    skip
+    dmns.add_exhibit("Gems and Minerals")
+    dmns.add_exhibit("Dead Sea Scrolls")
+    dmns.add_exhibit("IMAX")
+
+    assert_equal "Gems and Minerals", @dmns.exhibits
+    assert_equal "Dead Sea Scrolls", @dmns.exhibits
+    assert_equal "IMAX", @dmns.exhibits
+  end  
+
+
+
+
+  end
 
 
 
 
 
-end 
+
+end
