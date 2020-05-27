1096e6a70be74f4d47bb15a75eae8f7e65ac1d96
Amy Moller <amy.moller@yahoo.com>
2020-05-27 10:12:48 -0600

    Add first museum methods

---
+++ lib/museum.rb
@@ -0,0 +1,18 @@
+class Museum
+
+  attr_reader :name,
+              :exhibits,
+              :patrons
+
+  def initialize(name)
+    @name = name
+    @exhibits = []
+    @patrons = []
+  end
+
+
+
+
+
+
+end 
