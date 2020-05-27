55c09682ad4fae81022b96b70756d6a0d3e0a561
Amy Moller <amy.moller@yahoo.com>
2020-05-27 09:59:11 -0600

    Fix syntax and confirm all tests

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -6,36 +6,32 @@ require './lib/patron'
 class PatronTest < Minitest::Test
 
   def setup
-    skip
-    @patron_1 = Patron.new("Bob", 20)
+
+    @patron_1 = Patron.new({name: "Bob", spending_money: 20})
   end
 
   def test_it_exists
-    skip
+
     assert_instance_of Patron, @patron_1
   end
 
   def test_it_has_a_name
-    skip
+
     assert_equal "Bob", @patron_1.name
   end
 
   def test_it_has_spending_money
-    skip
+
     assert_equal 20, @patron_1.spending_money
   end
 
   def test_it_has_interests
-    skip
+
     patron_1.add_interest("Dead Sea Scrolls")
     patron_1.add_interest("Gems and Minerals")
 
     assert_equal "Dead Sea Scrolls", @patron_1.interests
     assert_equal "Gems and Minerals", @patron_1.interests
   end
-
-
-
-
 
 end
