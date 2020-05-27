ffd277f92a4a096b769178a31c4e34728d70f06d
Amy Moller <amy.moller@yahoo.com>
2020-05-27 09:46:50 -0600

    Add more patron tests

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -25,6 +25,15 @@ class PatronTest < Minitest::Test
     assert_equal 20, @patron_1.spending_money
   end
 
+  def test_it_has_interests
+    skip
+    patron_1.add_interest("Dead Sea Scrolls")
+    patron_1.add_interest("Gems and Minerals")
+
+    assert_equal "Dead Sea Scrolls", @patron_1.interests
+    assert_equal "Gems and Minerals", @patron_1.interests
+  end
+
 
 
 
